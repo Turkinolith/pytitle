49ec7bb9efaa28c33bdabe92d60d543d317156af
Larry <50931759+Turkinolith@users.noreply.github.com>
2019-12-17 13:39:03 -0700

    Update pytitle.py

--- pytitle.py
+++ pytitle.py
@@ -1,7 +1,7 @@
 import pyperclip
 
 print("lets build a title")
-usertext = input("type in a string:")
+usertext = input("type in a string: ")
 
 headerFooter =  "".join(["*****"] + (["*"] * len(usertext)) + ["*****"])
 titleLine = "".join(["**** "] + [usertext] + [" ****"])
